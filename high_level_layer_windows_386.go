ce84fb62ec289c0af698c02efa2c3cd03de37472
Semyon Maryasin <simeon@maryasin.name>
2020-11-07 20:46:57 +0300

    Change python lib path for win64

---
+++ high_level_layer_windows_386.go
@@ -0,0 +1,7 @@
+package python3
+
+/*
+  #cgo CFLAGS: -I/home/mars/.wine/drive_c/py3-32/include/
+  #cgo LDFLAGS: -L/home/mars/.wine/drive_c/py3-32/libs/ -lpython38
+*/
+import "C"
---
+++ high_level_layer_windows_amd64.go
@@ -0,0 +1,7 @@
+package python3
+
+/*
+  #cgo CFLAGS: -I/home/mars/.wine/drive_c/py38/include/
+  #cgo LDFLAGS: -L/home/mars/.wine/drive_c/py38/libs/ -lpython38
+*/
+import "C"
